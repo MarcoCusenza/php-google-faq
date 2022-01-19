9af151dfc7895a7fd9d698d99687af3a6555bd7e
Marco Cusenza <marco.cusenza.me@gmail.com>
2022-01-19 19:42:51 +0100

    footer

--- index.php
+++ index.php
@@ -127,7 +127,19 @@ $faqs = [
     </div>
   </main>
 
-  <footer></footer>
+  <footer>
+    <div class="container">
+      <ul>
+        <li><a href="#">Google</a></li>
+        <li><span>-</span></li>
+        <li><a href="#">Tutto su Google</a></li>
+        <li><span>-</span></li>
+        <li><a href="#">Privacy</a></li>
+        <li><span>-</span></li>
+        <li><a href="#">Termini</a></li>
+      </ul>
+    </div>
+  </footer>
 
 </body>
 
